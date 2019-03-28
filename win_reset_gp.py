09baf3161d6f8ca190487efeaacd6dff755d64da
SeaHOH <Seahoh@Gmail.com>
2019-03-28 17:47:29 +0800

    allow use with import

--- win_reset_gp.py
+++ win_reset_gp.py
@@ -1,7 +1,6 @@
 # coding:utf-8
 
 import os
-import sys
 import platform
 import locale
 import ctypes
@@ -15,8 +14,7 @@ def win32_notify( msg='msg', title='Title'):
 
 def reset_teredo():
     gp_split = b'[\x00'
-    gp_teredo = 'v6Transition\x00;Teredo'
-    gp_teredo = '\x00'.join(b for b in gp_teredo).encode()
+    gp_teredo = '\x00'.join(list('v6Transition\x00;Teredo')).encode()
 
     with open(gp_regpol_file, 'rb') as f:
         gp_regpol_old = f.read().split(gp_split)
@@ -24,23 +22,35 @@ def reset_teredo():
     gp_regpol_new = [gp for gp in gp_regpol_old if gp_teredo not in gp]
 
     if len(gp_regpol_new) != len(gp_regpol_old) and \
-            win32_notify(u'发现组策略 Teredo 设置，是否重置？', u'提醒'):
+            win32_notify(notice_text, notice_title):
         with open(gp_regpol_file, 'wb') as f:
             f.write(gp_split.join(gp_regpol_new))
         os.system(sysnative + '\\gpupdate /target:computer /force')
 
-if '__main__' == __name__:
+def main():
     if os.name != 'nt':
-        sys.exit(0)
+        return
 
     sysver = platform.version()
     if sysver < '6':
         # Teredo item was added to Group Policy starting with Windows Vista
-        sys.exit(0)
+        return
 
     windir = os.environ.get('windir')
     if not windir:
-        sys.exit(-1)
+        return
+
+    try:
+        zh_locale = locale.getdefaultlocale()[0] == 'zh_CN'
+    except:
+        zh_locale = None
+
+    if zh_locale:
+        notice_title = u'\u63d0\u9192'
+        notice_text = u'\u53d1\u73b0\u7ec4\u7b56\u7565 Teredo \u8bbe\u7f6e\uff0c\u662f\u5426\u91cd\u7f6e\uff1f'
+    else:
+        notice_title = 'Notice'
+        notice_text = 'Found Teredo settings in Group Policy, do you want to reset it?'
 
     sys64 = os.path.exists(windir + '\\SysWOW64')
     pe32 = platform.architecture()[0] == '32bit'
@@ -50,6 +60,9 @@ if '__main__' == __name__:
 
     if os.path.exists(gp_regpol_file):
         if not win32runas.is_admin():
-            win32runas.runas()
-            sys.exit(0)
+            win32runas.runas(os.path.abspath(__file__))
+            return
         reset_teredo()
+
+if '__main__' == __name__:
+    main()
